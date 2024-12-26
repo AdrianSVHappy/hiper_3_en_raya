afa594b502d7085f5db76d816d4fdf5b61048e70
Adrián Suárez Valdayo <adriansuarezvaldayo573@gmail.com>
2024-12-26 21:46:49 +0100

    En blanco

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  * <p>Esta clase represanta </p>
  *
  * @author Adrián Suárez Valdayo
- * @vesrion 26-dic-2024
+ * @version 26-dic-2024
  */
 public class Main {
     public static void main(String[] args) {
